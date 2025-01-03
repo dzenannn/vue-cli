ef73ea90412d278df64eebb36a1085cb3316dde9
dzenann <dzenan.dev@gmail.com>
2025-01-03 18:29:34 +0100

    conditional expressions

--- src/components/Rendering.vue
+++ src/components/Rendering.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>Rendering Component</h1>
-    <div v-for="{ name, id } in myList" :key="id">
-      <p style="text-align: start">{{ id }}. {{ name }}</p>
+    <div v-for="{ name, id, isActive } in myList" :key="id">
+      <h1 v-if="isActive" :class="esnaf">
+        {{ name }}
+      </h1>
     </div>
   </div>
 </template>
@@ -12,29 +14,43 @@ export default {
   methods: {},
   data() {
     return {
+      esnaf: "header",
       myList: [
         {
           id: 1,
           name: "John",
+          isActive: true,
         },
         {
           id: 2,
           name: "Dzeno",
+          isActive: false,
         },
         {
           id: 3,
           name: "Jack",
+          isActive: true,
         },
         {
           id: 4,
           name: "William",
+          isActive: false,
         },
         {
           id: 5,
           name: "Paul",
+          isActive: true,
         },
       ],
     };
   },
 };
 </script>
+<style scoped>
+.header {
+  border: 1px solid orangered;
+  background-color: orangered;
+  padding: 1rem;
+  color: black;
+}
+</style>
