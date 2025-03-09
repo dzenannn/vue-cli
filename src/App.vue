203a48109296dd9a832d5fd5d6932195d570f57b
dzenannn <dzenan.dev@gmail.com>
2025-03-09 16:29:02 +0100

    forms in vue (template only)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>{{ total }}</h1>
+    <!-- <h1>{{ total }}</h1>
     <h3>{{ totalResult }}</h3>
-    <button @click="total += 1">Click</button>
+    <button @click="total += 1">Click</button> -->
+    <Form />
   </div>
   <!-- <keep-alive> <component :is="activeComponent" /></keep-alive> -->
   <!-- keep-alive sluzi da nam sačuva sve podatke sa određene komponente -->
@@ -10,9 +11,11 @@
 </template>
 
 <script>
+import Form from "@/components/Form.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { Form },
   computed: {
     totalResult() {
       console.log("Computed property rendered");
@@ -36,7 +39,6 @@ body {
   padding: 0;
   box-sizing: border-box;
   background-color: #8f5e7f;
-  margin-top: 50px;
 }
 #app {
   font-family: "Poppins";
@@ -46,17 +48,5 @@ body {
 
 div {
   place-items: center;
-}
-
-button {
-  cursor: pointer;
-  height: 40px;
-  display: flex;
-  place-items: center;
-  border: none;
-  border-radius: 10px;
-  background-color: #2c3e50;
-  color: #fff;
-  padding: 8px;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,130 @@
+<script>
+export default {
+  name: "Form",
+};
+</script>
+
+<template>
+  <div class="main">
+    <div class="wrapper">
+      <form class="myForm">
+        <div class="inputDiv">
+          <label for="nameInput">Name</label>
+          <input type="text" id="nameInput" placeholder="Enter your name" />
+        </div>
+        <div class="inputDiv">
+          <label for="surnameInput">Surname</label>
+          <input
+            type="text"
+            id="surnameInput"
+            placeholder="Enter your surname"
+          />
+        </div>
+        <div class="inputDiv">
+          <label for="emailInput">E-mail</label>
+          <input type="email" id="emailInput" placeholder="Enter your email" />
+        </div>
+        <div class="inputDiv">
+          <label for="country">Country</label>
+          <select name="country" id="country">
+            <option>England</option>
+            <option>Serbia</option>
+            <option>Germany</option>
+          </select>
+        </div>
+        <div class="inputDiv">
+          <label for="address">Address</label>
+          <textarea name="address" id="address" rows="5"></textarea>
+        </div>
+        <div>
+          <h4>Hobbies</h4>
+          <div class="checkboxDiv">
+            <div>
+              <label for="checkbox1">Football</label>
+              <input type="checkbox" id="checkbox1" />
+            </div>
+            <div>
+              <label for="checkbox2">Basketball</label>
+              <input type="checkbox" id="checkbox2" />
+            </div>
+            <div>
+              <label for="checkbox3">Gaming</label>
+              <input type="checkbox" id="checkbox3" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h4>Gender</h4>
+          <div class="checkboxDiv">
+            <div>
+              <label for="radio1">Male</label>
+              <input type="radio" name="gender" id="radio1" />
+            </div>
+            <div>
+              <label for="radio2">Female</label>
+              <input type="radio" name="gender" id="radio2" />
+            </div>
+          </div>
+        </div>
+        <button type="submit">Send data</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  width: 140%;
+  margin: 25%;
+  display: flex;
+  justify-content: center;
+}
+.wrapper {
+  width: 80%;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 1px 3px 3px 1px;
+  background-color: rgba(245, 171, 183, 0.681);
+}
+
+.myForm {
+  display: flex;
+  flex-direction: column;
+}
+
+.inputDiv {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 5px;
+}
+
+.myForm button {
+  border: none;
+  padding: 10px;
+  margin-top: 15%;
+  border-radius: 10px;
+  color: white;
+  background-color: #161616;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+}
+
+.myForm button:hover {
+  background-color: #2e2e2e;
+}
+
+input {
+  padding: 6px;
+}
+
+select {
+  padding: 6px;
+  width: 100%;
+}
+
+.checkboxDiv {
+  display: flex;
+}
+</style>
